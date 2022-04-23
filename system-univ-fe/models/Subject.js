69cecfd80a776b4ce736981e2142d5543c8df81e
Merouane Mohamed Elamine <59193074+merouane2000@users.noreply.github.com>
2022-04-23 19:40:18 +0000

    get student api

--- system-univ-fe/models/Subject.js
+++ system-univ-fe/models/Subject.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const SubjectSchema = new Schema({
   _id: { type: String, default: uuid.v4, required: true },
   subjectName: { type: String, required: true },
-  subjectoFSemaster: { type: Number },
+  subjectoFSemaster: { type: String },
   coif: { type: Number, min: 1, max: 5 },
   credit: { type: Number, min: 1, max: 5 },
   exam: { type: String },
--- system-univ-fe/routes/api.js
+++ system-univ-fe/routes/api.js
@@ -12,6 +12,11 @@ router.get("/get-subject", (req, res) => {
     res.send(Subjects);
   });
 });
+router.get("/get-student", (req, res) => {
+  Student.find({}).then((Subjects) => {
+    res.send(Subjects);
+  });
+});
 
 router.post("/admin/login", (req, res) => {
   Admin.create({
@@ -53,83 +58,110 @@ router.post("/subject", (req, res) => {
         IsCreated: false,
       });
     });
-
-  console.log(req.body);
 });
 
 router.post("/student", (req, res) => {
   const { datas, values } = req.body;
+  console.log(values);
+  Student.create({
+    name: values.FirstName,
+    familyName: values.FamilyName,
+    registrationNumber: values.RegistrationNumber,
+    classRoom: values.ClassRoom,
+    pointes: [
+      {
+        subjectName: values.Points.subjectName,
+        exam: values.Points.Exam,
+        TD: values.Points.TD,
+        TP: values.Points.TP,
+      },
+    ],
+  })
+    .then(() => {
+      res.send({
+        IsCreated: true,
+      });
+    })
+    .catch((e) => {
+      res.send({
+        msg: e,
+        IsCreated: false,
+      });
+    });
+
+  // if(Student.find({registrationNumber:values.RegistrationNumber}) = true){
+
+  //   Student.update({registrationNumber:values.RegistrationNumber},
+  // {
+  //   pointes: [{
+  //     ...values.pointes,
+  //       subjectName: values.Points.subjectName,
+  //       exam: values.Points.Exam,
+  //       TD: values.Points.TD,
+  //       TP: values.Points.TP,
+  //   }],
+  // }
+  // ).then(() => {
+  //   res.send({
+  //     IsUpdated: true,
+  //   });
+  // })
+  // .catch((e) => {
+  //   res.send({
+  //     msg: e,
+  //     IsUpdated: false,
+  //   });
+  // });
+ 
+
+  // } else {
+
+  // }
+
+  // for (let i = 0; i < datas.length; i++) {
+  //   if (values.Points.subjectName === datas[i].subjectName) {
+  //     if (
+  //       datas[i].exam == true &&
+  //       datas[i].td == true &&
+  //       datas[i].tp == true
+  //     ) {
+  //       values.Points.subjectMoyenne =
+  //         values.Points.Exam * 0.6 +
+  //         values.Points.TP * 0.2 +
+  //         values.Points.TD * 0.2;
+  //     } else if (
+  //       datas[i].exam == true &&
+  //       datas[i].td == true &&
+  //       datas[i].tp == false
+  //     ) {
+  //       values.Points.subjectMoyenne =
+  //         values.Points.Exam * 0.6 +
+  //          values.Points.TD * 0.4 +
+  //           values.Points.TP * 0 ;
+  //     } else if (
+  //       datas[i].exam == true &&
+  //       datas[i].td == false &&
+  //       datas[i].tp == true
+  //     ) {
+  //       values.Points.subjectMoyenne =
+  //         values.Points.Exam * 0.6 +
+  //          values.Points.TP * 0.4 +
+  //          values.Points.TD * 0 ;
+  //     } else if (
+  //       datas[i].exam == true &&
+  //       datas[i].td == true &&
+  //       datas[i].tp == true
+  //     ) {
+  //       values.Points.subjectMoyenne =
+  //        values.Points.Exam*1 +
+  //        values.Points.TP * 0 +
+  //          values.Points.TD * 0;
+  //     }
+  //   }
+  // }
+
 
   
-    // for (let i = 0; i < datas.length; i++) {
-    //   if (values.Points.subjectName === datas[i].subjectName) {
-    //     if (
-    //       datas[i].exam == true &&
-    //       datas[i].td == true &&
-    //       datas[i].tp == true
-    //     ) {
-    //       values.Points.subjectMoyenne =
-    //         values.Points.Exam * 0.6 +
-    //         values.Points.TP * 0.2 +
-    //         values.Points.TD * 0.2;
-    //     } else if (
-    //       datas[i].exam == true &&
-    //       datas[i].td == true &&
-    //       datas[i].tp == false
-    //     ) {
-    //       values.Points.subjectMoyenne =
-    //         values.Points.Exam * 0.6 +
-    //          values.Points.TD * 0.4 +
-    //           values.Points.TP * 0 ;
-    //     } else if (
-    //       datas[i].exam == true &&
-    //       datas[i].td == false &&
-    //       datas[i].tp == true
-    //     ) {
-    //       values.Points.subjectMoyenne =
-    //         values.Points.Exam * 0.6 +
-    //          values.Points.TP * 0.4 +
-    //          values.Points.TD * 0 ;
-    //     } else if (
-    //       datas[i].exam == true &&
-    //       datas[i].td == true &&
-    //       datas[i].tp == true
-    //     ) {
-    //       values.Points.subjectMoyenne =
-    //        values.Points.Exam*1 +
-    //        values.Points.TP * 0 +
-    //          values.Points.TD * 0;
-    //     }
-    //   }
-    // }
-
-    Student.create({
-      name: values.FirstName,
-      familyName: values.FamilyName,
-      registrationNumber: values.RegistrationNumber,
-      classRoom: values.ClassRoom,
-      pointes: [
-        {
-          subjectName: values.Points.subjectName,
-          exam: values.Points.Exam,
-          TD: values.Points.TD,
-          TP: values.Points.TP,
-          // subjectMoyenne: values.Points.subjectMoyenne,
-        },
-      ],
-    })
-      .then(() => {
-        res.send({
-          IsCreated: true,
-        });
-      })
-      .catch((e) => {
-        res.send({
-          msg: e,
-          IsCreated: false,
-        });
-      });
-
 
   // if (values != null) {
   //   const filter = { registrationNumber: values.RegistrationNumber };
